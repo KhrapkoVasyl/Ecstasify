6b1e9bcd320ff2d2027f10553fc77e011daa9c3f
Bogdan-Zinovij <76776361+Bogdan-Zinovij@users.noreply.github.com>
2023-06-07 23:56:33 +0300

    chore: set up postgres database. Closes #10

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppConfigModule } from './config';
+import { DatabaseModule } from './systems/database';
 
 @Module({
-  imports: [AppConfigModule],
+  imports: [AppConfigModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- server/src/config/app-config.service.ts
+++ server/src/config/app-config.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { join } from 'path';
+import { NodeModeEnum } from 'src/common/enums';
 
 @Injectable()
 export class AppConfigService {
@@ -17,5 +20,31 @@ export class AppConfigService {
     } catch {
       return value;
     }
+  }
+
+  public isMode(mode: NodeModeEnum): boolean {
+    return this.get('NODE_ENV') === mode;
+  }
+
+  getPostgresConfig(): TypeOrmModuleOptions {
+    return {
+      type: this.get<'postgres'>('TYPEORM_TYPE'),
+      name: this.get('TYPEORM_CONNECTION_NAME'),
+      host: this.get('TYPEORM_HOST'),
+      port: this.get('TYPEORM_PORT'),
+      cache: this.get('TYPEORM_CACHE'),
+      logging: this.get('TYPEORM_LOGGING'),
+      database: this.get<string>('TYPEORM_DATABASE'),
+      username: this.get('TYPEORM_USERNAME'),
+      password: this.get<string>('TYPEORM_PASSWORD'),
+      extra: {
+        ssl: this.get('TYPEORM_SSL'),
+      },
+      dropSchema: this.get('TYPEORM_DROP_SCHEMA'),
+      synchronize: this.get('TYPEORM_SYNCHRONIZE'),
+      migrationsRun: this.get('TYPEORM_MIGRATIONS_RUN'),
+      entities: [join(__dirname, '../modules/**/*.entity.{ts,js}')],
+      migrations: [join(__dirname, '../systems/database/migrations/*.{ts,js}')],
+    };
   }
 }
