221966976eabbbbebbf89f88c2f12d54db93be8b
Bogdan-Zinovij <76776361+Bogdan-Zinovij@users.noreply.github.com>
2023-06-08 22:40:44 +0300

    add 'feature' entity

---
+++ server/src/modules/features/feature.entity.ts
@@ -0,0 +1,35 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  ManyToMany,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { SubscriptionPlanEntity } from '../subscription-plans/subscription-plan.entity';
+
+@Entity({ name: 'features' })
+export class FeatureEntity {
+  @ApiProperty()
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @ApiProperty({ maxLength: 50 })
+  @Column({ length: 50 })
+  name: string;
+
+  @ManyToMany(
+    () => SubscriptionPlanEntity,
+    (subscriptionPlan) => subscriptionPlan.features,
+  )
+  subscriptionPlans: SubscriptionPlanEntity[];
+
+  @ApiProperty()
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @ApiProperty()
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
