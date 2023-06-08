46deb1b27e5620b7f7a65734d453ab324745cbc5
Bogdan-Zinovij <76776361+Bogdan-Zinovij@users.noreply.github.com>
2023-06-08 22:39:22 +0300

    add 'user' entity

---
+++ server/src/modules/users/user.entity.ts
@@ -0,0 +1,47 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { SubscriptionPlanEntity } from '../subscription-plans/subscription-plan.entity';
+
+@Entity({ name: 'users' })
+export class UserEntity {
+  @ApiProperty()
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @ApiProperty({ maxLength: 20 })
+  @Column({ length: 20 })
+  name: string;
+
+  @ApiProperty({ maxLength: 16 })
+  @Column({ length: 16 })
+  password: string;
+
+  @ApiProperty({ maxLength: 254 })
+  @Column({ length: 254 })
+  email: string;
+
+  @ApiProperty()
+  @Column()
+  role: number;
+
+  @ManyToOne(
+    () => SubscriptionPlanEntity,
+    (subscriptionPlan) => subscriptionPlan.users,
+  )
+  subscriptionPlan: SubscriptionPlanEntity;
+
+  @ApiProperty()
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @ApiProperty()
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
