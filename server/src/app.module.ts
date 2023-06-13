fe2ebc07f0c59cb6418bb4e66e4482d085387848
Bogdan-Zinovij <76776361+Bogdan-Zinovij@users.noreply.github.com>
2023-06-13 10:56:15 +0300

    implement crud for authors module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { AppConfigModule } from './config';
 import { DatabaseModule } from './systems/database';
 import { FeaturesModule } from './modules/features';
+import { AuthorsModule } from './modules/authors';
 
 @Module({
-  imports: [AppConfigModule, DatabaseModule, FeaturesModule],
+  imports: [AppConfigModule, DatabaseModule, FeaturesModule, AuthorsModule],
   controllers: [AppController],
   providers: [AppService],
 })
