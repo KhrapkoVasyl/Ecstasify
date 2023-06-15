aed0b13f90a90524a80273cbd39dc9d4886a85b4
Bogdan-Zinovij <76776361+Bogdan-Zinovij@users.noreply.github.com>
2023-06-15 12:52:06 +0300

    add method for update refresh token

--- server/src/modules/refresh-token/refresh-tokens.service.ts
+++ server/src/modules/refresh-token/refresh-tokens.service.ts
@@ -4,6 +4,7 @@ import { Repository } from 'typeorm';
 import { RefreshTokenEntity } from './refresh-token.entity';
 import { refreshTokensServiceErrorMessages } from './refresh-tokens.constants';
 import { BaseService } from 'src/common/services';
+import * as ms from 'ms';
 import { AppConfigService } from 'src/config/app-config.service';
 import { UsersService } from '../users/users.service';
 
@@ -16,5 +17,30 @@ export class RefreshTokensService extends BaseService<RefreshTokenEntity> {
     private usersService: UsersService,
   ) {
     super(refreshTokenEntityRepository, refreshTokensServiceErrorMessages);
+  }
+
+  async updateRefreshToken(userId: string, refreshToken: string) {
+    const user = await this.usersService.findOne({ id: userId });
+
+    const tokenLifetime = this.appConfigService.get('JWT_REFRESH_EXPIRES_IN');
+    const tokenDuration = ms(tokenLifetime);
+    const currentDateTime = new Date();
+    const expiresAt = new Date(currentDateTime.getTime() + tokenDuration);
+
+    const refreshTokenModel: Partial<RefreshTokenEntity> = {
+      user,
+      value: refreshToken,
+      expiresAt,
+    };
+
+    const existingRefreshToken = await this.findOne({
+      user: { id: userId },
+    }).catch(() => null);
+
+    if (!existingRefreshToken) {
+      return this.createOne(refreshTokenModel);
+    }
+
+    return this.updateOne({ id: existingRefreshToken.id }, refreshTokenModel);
   }
 }
