e7697565508ba53397dad72bb57b07a87b5c0b38
KhrapkoVasyl <khrapko2002@gmail.com>
2023-06-22 00:03:00 +0300

    feat(files): implement getter for getting src to file in file entity

--- server/src/modules/files/file.entity.ts
+++ server/src/modules/files/file.entity.ts
@@ -1,4 +1,7 @@
+import { ConfigService } from '@nestjs/config';
 import { ApiProperty } from '@nestjs/swagger';
+import { Expose } from 'class-transformer';
+import { AppConfigService } from 'src/config/app-config.service';
 import {
   PrimaryGeneratedColumn,
   CreateDateColumn,
@@ -7,6 +10,8 @@ import {
   Column,
   Entity,
 } from 'typeorm';
+
+const appConfigService = new AppConfigService(new ConfigService());
 
 @Entity('files')
 export class FileEntity extends BaseEntity {
@@ -33,6 +38,14 @@ export class FileEntity extends BaseEntity {
   @ApiProperty({ type: 'string', maxLength: 256, required: true })
   @Column({ type: 'varchar', length: 256 })
   public readonly mimetype: string;
+
+  @Expose()
+  @ApiProperty({ readOnly: true })
+  get src(): string {
+    if (!this.filePath) return null;
+    const fileUri = new URL(this.filePath, appConfigService.get('CDN'));
+    return fileUri.toString();
+  }
 
   @ApiProperty({ readOnly: true })
   @CreateDateColumn({
