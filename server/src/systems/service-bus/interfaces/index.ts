13117a8042c60cbbb5d7772442ad8d364aa2f9ec
KhrapkoVasyl <khrapko2002@gmail.com>
2024-01-06 03:04:05 +0200

    chore: remove unused message brocker logic

--- server/src/systems/service-bus/interfaces/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './publisher.interface';
-export * from './subscriber.interface';
--- server/src/systems/service-bus/interfaces/subscriber.interface.ts
+++
@@ -1,4 +0,0 @@
-export interface ISubscriber {
-  subscribe(): Promise<void>;
-  get ids(): string[];
-}
